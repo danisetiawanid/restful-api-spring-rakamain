581835dcd5f0553fbea6dce740c2be592bfb3bca
Muhamad Dani Setiawan <muhamaddanisetiawan@gmail.com>
2024-03-19 10:49:08 +0700

    add response and patch method

--- src/main/java/com/example/restfulapidani/controller/ProductController.java
+++ src/main/java/com/example/restfulapidani/controller/ProductController.java
@@ -1,9 +1,12 @@
 package com.example.restfulapidani.controller;
 
 import com.example.restfulapidani.model.Product;
+import com.example.restfulapidani.model.Response;
 import com.example.restfulapidani.repository.ProductRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -11,48 +14,75 @@ public class ProductController {
 
     private final ProductRepository productRepository;
 
-    public ProductController( ProductRepository productRepository){
+    public ProductController(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     @GetMapping(value = "product", produces = "application/json")
-    public Iterable<Product> getProduct(){
+    public Iterable<Product> getProduct() {
         return this.productRepository.findAll();
     }
 
 
     @PostMapping(value = "/product", consumes = "application/json", produces = "application/json")
-    public Product createProduct(@RequestBody Product newProduct){
-
+    public Product createProduct(@RequestBody Product newProduct) {
         return this.productRepository.save(newProduct);
     }
 
     @DeleteMapping(value = "/product/{id}", produces = "application/json")
 
-    public ResponseEntity<String> deleteProduct(@PathVariable Long id){
+//    public ResponseEntity<String> deleteProduct(@PathVariable Long id){
+//        if (this.productRepository.findById(id).isEmpty()) {
+//            return ResponseEntity.status(400).body("Product Not Found");
+//        }
+//
+//        this.productRepository.deleteById(id);
+//        return ResponseEntity.ok("Deleted");
+//    }
+
+    public ResponseEntity<Response> deleteProduct(@PathVariable Long id) {
         if (this.productRepository.findById(id).isEmpty()) {
-            return ResponseEntity.status(400).body("Product Not Found");
+            return ResponseEntity.badRequest().body(new Response("Product not found"));
         }
 
+
         this.productRepository.deleteById(id);
-        return ResponseEntity.ok("Deleted");
+        Response response = new Response("Delete Success", null);
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping(value = "/product/{id}", produces = "application/json")
-    public ResponseEntity<String> updateProduct(@PathVariable Long id, @RequestBody Product updateProduct){
+    public ResponseEntity<Response> updateProduct(@PathVariable Long id, @RequestBody Product updateProduct) {
+
         if (this.productRepository.findById(id).isEmpty()) {
-            return ResponseEntity.status(400).body("Product not Found");
+//            return ResponseEntity.status(400).body("Product not Found");
+            return ResponseEntity.badRequest().body(new Response("Product Not Found"));
         }
         updateProduct.setId(id);
-        this.productRepository.save(updateProduct);
-        return ResponseEntity.ok("Success");
+        Response response = new Response("Success", this.productRepository.save(updateProduct));
+        return ResponseEntity.ok(response);
     }
 
 
+    @PatchMapping(value = "/product/{id}", produces = "application/json")
 
-    @PatchMapping(value = "/product", produces = "application/json")
+    public ResponseEntity<String> patchProduct(@PathVariable Long id, @RequestBody Product updateProduct) {
+        Optional<Product> productOptional = this.productRepository.findById(id);
+        if (productOptional.isEmpty()) {
+            return ResponseEntity.status(404).body("Product Not Found");
+        }
+        Product product = productOptional.get();
+        if (updateProduct.getName() != null) {
+            product.setName(updateProduct.getName());
+        }
+        if (updateProduct.getQty() != null) {
+            product.setQty(updateProduct.getQty());
+        }
+        if (updateProduct.getPrice() != null) {
+            product.setPrice(updateProduct.getPrice());
+        }
+        this.productRepository.save(product);
+        return ResponseEntity.ok("Patch Success");
 
-    public String patchProduct(){
-        return "Patch Product";
     }
 }
---
+++ src/main/java/com/example/restfulapidani/model/Response.java
@@ -0,0 +1,34 @@
+package com.example.restfulapidani.model;
+
+public class Response {
+    private String status;
+
+    private Object data;
+
+    public Response(String status) {
+        this.status = status;
+    }
+
+    public Response(String status, Object data) {
+        this.status = status;
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+
+}
